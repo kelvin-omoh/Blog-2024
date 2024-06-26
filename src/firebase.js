9e40a99a246ff957c3b6b4988aade1c8000802b4
enaikele omoh kelvin <enaikeleomoh@gmail.com>
2024-06-26 10:53:46 +0100

    first commit

--- src/firebase.js
+++ src/firebase.js
@@ -4,7 +4,13 @@ import { getStorage } from "firebase/storage";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  // firebase
+  apiKey: "AIzaSyCFnzViSGI36LM8UKIIbqenit0th4R97kA",
+  authDomain: "voltaic-charter-393405.firebaseapp.com",
+  projectId: "voltaic-charter-393405",
+  storageBucket: "voltaic-charter-393405.appspot.com",
+  messagingSenderId: "900540364932",
+  appId: "1:900540364932:web:70a47472256ba8112f2ad1",
+  measurementId: "G-D364BKDCRL"
 };
 
 const app = initializeApp(firebaseConfig);
